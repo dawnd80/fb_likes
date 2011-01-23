0e57ed3fa01f6dca72156e6418cbe6df0fe784c7
Dixie Xue <dixie.xue@gmail.com>
2011-01-23 00:19:41 -0800

    show user is logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def new
-    #@user = User.new
+    @graph = Koala::Facebook::GraphAPI.new
+    @user = @graph.get_object("me")
     respond_to do |format|
       format.html
     end
