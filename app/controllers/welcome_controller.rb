56157e9e6df37786aa3a8a05d15d5ff86ae85170
Dixie Xue <dixie.xue@gmail.com>
2011-01-23 00:26:02 -0800

    fix access cookies

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,17 @@
 class WelcomeController < ApplicationController
+  before_filter :ensure_facebook_gapi
+
   def new
-    @graph = Koala::Facebook::GraphAPI.new
-    @user = @graph.get_object("me")
+    if access_cookies
+      @user = @graph.get_object("me")
+    end
     respond_to do |format|
       format.html
     end
+  end
+  
+  protected
+  def ensure_facebook_gapi
+    @graph = Koala::Facebook::GraphAPI.new
   end
 end
