e85fee9efb573473e49728143d11daa5376314b1
Dixie Xue <dixie.xue@gmail.com>
2011-01-23 08:45:11 -0800

    test

--- public/javascripts/welcome.js
+++ public/javascripts/welcome.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
   FB.init({appId: '185076204847204', status: true, cookie: true, xfbml: true});
-  FB.Event.subscribe('auth.login', function(response) { window.location.reload(); });
+  FB.Event.subscribe('auth.login', function(response) { window.location.href = $('#callback-url').text(); });
 });
